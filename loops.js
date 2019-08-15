9055171f2e1f69bf03d56271db9db8cc04b81ee7
M C <miguelcarmona996@gmail.com>
2019-08-15 21:37:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function whileLoop(n){
 function maybeTrue() {
   return Math.random() >= 0.5
 }
-function doWhileLoop(array){
+function doWhileLoop(num){
 do {
   array.pop()
 } while (array.length>0 && maybeTrue())
